730169665d1846bcf05a6f4948174b346bd4cf1f
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2022-06-09 21:48:34 -0300

    Tokens Alterados
    Se modifican los tokens por seguridad, estos no son los originales.

--- src/Helpers/publicTokens.js
+++ src/Helpers/publicTokens.js
@@ -1,4 +1,4 @@
 export const PUBLIC_TOKENS = {
-    mapbox:'pk.eyJ1IjoiY3Jpc3RpYW4wMjExOTUiLCJhIjoiY2t6ZzZvOXBnMDQwZDJ2bXA0dmtxeXNzdCJ9.TA4s6h3479ID2a4pa3evgQ',
-    openweather: '5d895cc1600abe918265bc31831d467f'
+    mapbox:'pk.eyJ1IjoiY3Jpc3RpYW4wMjExOTUiLCJhIjoiY2t6ZzZvOXBnMDQwZDJ2bXA0dmtxeXNzdCJ9.TA4s6h3479ID2a4pa3evgQB3',
+    openweather: '5d895cc1600abe918265bc31831d467fA2'
 }
